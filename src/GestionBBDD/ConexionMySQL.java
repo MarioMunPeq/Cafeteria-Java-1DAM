ed6700a5d710b2fc4574be74014c1e76916902a2
Snakeyesmp <snakeyes271101@gmail.com>
2023-05-09 17:35:05 +0200

    Unificacion de archivos de BBDD, inicio de conexiones JAVA con SQL

--- src/GestionBBDD/ConexionMySQL.java
+++ src/GestionBBDD/ConexionMySQL.java
@@ -1,43 +1,61 @@
 package GestionBBDD;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// Se importa la librería SQL 
 
 public class ConexionMySQL {
 
-    public static void main(String[] args) {
+    public static void enviarComandoSQL(String SentenciaSQL) {
 
-        String url = "jdbc:mysql://localhost:3306/Cafeteria";
-        String usuario = "root";
-        String password = "admin";
-
+        String url = "jdbc:mysql://localhost:3306/Cafeteria"; // La dirección de la base de datos(libreria de MySQL,
+                                                              // localhost es donde se ejecuta la base de datos, 3306 es
+                                                              // el puerto, y Cafetería el nombre de la base de datos )
+        String usuario = "root"; // El nombre de usuario con el que se va a acceder a la base de datos
+        String password = "admin123@"; // La contraseña usuario !!!!!! OJO QUE LA CONTRASEÑA SEA LA CORRECTA
         try {
             // Cargar el controlador JDBC de MySQL
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Este comando carga el driver MySQL en la clase
             // Establecer la conexión con la base de datos
-            Connection conexion = DriverManager.getConnection(url, usuario, password);
-
-            // Crear una sentencia SQL
-            String sql = "SELECT * FROM productos";
-
+            Connection conexion = DriverManager.getConnection(url, usuario, password); // Utilizas el driver MySQL y te
+                                                                                       // conectas a la base de datos
+                                                                                       // utilizando la ruta de la
+                                                                                       // propia base, el usuario y la
+                                                                                       // contraseña correspondientes
             // Ejecutar la sentencia SQL
             Statement sentencia = conexion.createStatement();
-            ResultSet resultado = sentencia.executeQuery(sql);
+            ResultSet resultado = sentencia.executeQuery(SentenciaSQL); // se manda a la base de datos la variable
+                                                                        // "SQL", que es
+                                                                        // el String de entrada del método
 
-            // Recorrer el resultado y mostrar los datos
+            // Se recorre el resultado
+            /* 
             while (resultado.next()) {
-                int id = resultado.getInt("id");
-                String nombre = resultado.getString("nombre");
-                float precio = resultado.getFloat("precio");
-                String alergeno = resultado.getString("alergeno");
-                int stock = resultado.getInt("stock");
-                System.out.println("Producto: " + nombre + " Precio: " + precio);
+                int id = resultado.getInt("id"); // Se busca en la tabla una columna que se llame 'id' y se asocia a una
+                                                 // variable con su mismo nombre y tipo
+                String nombre = resultado.getString("nombre"); // Se busca en la tabla una columna que se llame "nombre"
+                                                               // y se asocia a una variable con su mismo nombre y tipo
+                float precio = resultado.getFloat("precio"); // Se busca en la tabla una columna que se llame "precio" y
+                                                             // se asocia a una variable con su mismo nombre y tipo
+                String alergeno = resultado.getString("alergeno"); // Se busca en la tabla una columna que se llame
+                                                                   // "alergeno" y se asocia a una variable con su mismo
+                                                                   // nombre y tipo
+                int stock = resultado.getInt("stock"); // Se busca en la tabla una columna que se llame "stock" y se
+                                                       // asocia a una variable con su mismo nombre y tipo
             }
-
-            // Cerrar la conexión
-            conexion.close();
+            */
+            conexion.close(); // Cerrar la conexión
+        } catch (SQLException sqlException) {
+            System.err.println("Ha habido un error relacionado con MySQL");
+        } catch (ClassNotFoundException classNotFoundException) {
+            System.err.println("No se ha podido encontrar el driver para conectar con MySQL");
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println("Error sin identificar");
         }
+
     }
 }
---
+++ src/GestionBBDD/JavaParaIniciarLaBaseDeDatos.java
@@ -0,0 +1,51 @@
+package GestionBBDD;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JavaParaIniciarLaBaseDeDatos {
+
+    /**
+     * Metodo que crea la base de datos y le asigna los valores iniciales.
+     * Debe ser llamado nada más iniciarse la apliación
+     * 
+     */
+    public static void iniciarBBDD() {
+
+        String url = "jdbc:mysql://localhost:3306/Cafeteria";
+        String user = "root";
+        String password = "admin123@";
+        String archivoSql = "/BBDD/CreacionBBDD.sql";
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Crear la conexión a la base de datos
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            // Crear el objeto Statement
+            Statement stmt = conn.createStatement();
+
+            // Abrir el archivo SQL y leerlo línea por línea
+            BufferedReader reader = new BufferedReader(new FileReader(archivoSql));
+            String lineaSql = reader.readLine();
+            while (lineaSql != null) {
+                // Ejecutar cada línea del archivo SQL
+                stmt.execute(lineaSql);
+                lineaSql = reader.readLine();
+            }
+
+            // Cerrar el archivo y los objetos Statement y Connection
+            reader.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            // Manejar cualquier excepción que pueda surgir al ejecutar los comandos SQL
+            System.out.println("Excepcion sin manejar");
+        }
+    }
+
+}
