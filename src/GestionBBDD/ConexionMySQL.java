b85c8001e75a6e2315ffce6a57a4b106d726b840
Snakeyesmp <snakeyes271101@gmail.com>
2023-05-16 17:40:17 +0200

    Modificacion del acceso a la BBDD(otra vez)

--- src/GestionBBDD/ConexionMySQL.java
+++ src/GestionBBDD/ConexionMySQL.java
@@ -9,6 +9,8 @@ import java.sql.Statement;
 
 public class ConexionMySQL {
 
+    final static Statement SENTENCIA = conexionBaseDatos();
+
     public static Statement conexionBaseDatos() {
         final String URL = "jdbc:mysql://localhost:3306/Cafeteria"; // Direccion BBDD
         final String USUARIO = "root"; // Nombre usuario acceder BBDD
@@ -21,117 +23,79 @@ public class ConexionMySQL {
             // Establecer conexión con BBDD
             Connection conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
 
-            Statement sentencia = conexion.createStatement();
+            return conexion.createStatement();
 
-            return sentencia;
         } catch (ClassNotFoundException e) {
             // Manejar la excepción de clase no encontrada
-            System.err.println("Error: " + e);
+            System.err.println("Error de clase: " + e);
         } catch (SQLException e) {
             // Manejar la excepción de SQL
-            System.err.println("Error:" + e);
+            System.err.println("Error de SQL:" + e);
         }
 
         return null; // Retornar null en caso de error
     }
 
-    public static void consultarProductos(String consulta) throws SQLException {
+    public static int sacarIdProductos(String nombreProducto) throws SQLException {
 
-        // Se crea un dato de tipo sentencia con el metodo para conectarme a la BBDD
-        Statement sentencia = conexionBaseDatos();
-
-        ResultSet resultadoConsulta = sentencia.executeQuery(consulta);
-
-        System.out.println("id | nombre | precio | alergeno | stock");
-
-        while (resultadoConsulta.next()) {
-            // Recoger el id del producto
-            int id = resultadoConsulta.getInt("id");
-            // Recoger datos del nombre
-            String nombre = resultadoConsulta.getString("nombre");
-            // Recoger datos de precio
-            double precio = resultadoConsulta.getDouble("precio");
-            // Recoger datos de alergeno
-            String alergeno = resultadoConsulta.getString("alergeno");
-            // Recoger datos de stock
-            int stock = resultadoConsulta.getInt("stock");
-            // Prueba para imprimir los datos
-            System.out.println(id + "" + nombre + "" + precio + "" + alergeno + "" + stock);
-        }
+        ResultSet resultadoConsulta = SENTENCIA.executeQuery(CONSULTA_SQL + nombreProducto);
+        // Se devuelve un id con la consulta que se le ha metido
+        return resultadoConsulta.getInt("id");
 
     }
 
-    public static void consultarTickets(String consulta) throws SQLException {
-        // Se crea un dato de tipo sentencia con el metodo para conectarme a la BBDD
-        Statement sentencia = conexionBaseDatos();
+    public static String datosProductoParaTicket(int id) throws SQLException {
 
-        ResultSet resultadoConsulta = sentencia.executeQuery(consulta);
+        ResultSet resultadoConsulta = SENTENCIA.executeQuery("select nombre from productos where id=" + id);
 
-        System.out.println("id | pagado | pagadoTarjeta | caja_id");
+        // Recoger datos del nombre
+        String nombre = resultadoConsulta.getString("nombre");
+        // Recoger datos de precio
+        double precio = resultadoConsulta.getDouble("precio");
 
-        while (resultadoConsulta.next()) {
-            // Recoger el id del ticket
-            int id = resultadoConsulta.getInt("id");
-            // Recoger datos de si está pagado
-            boolean pagado = resultadoConsulta.getBoolean("pagado");
-            // Recoger dato de si se ha pagado con tarjeta
-            boolean pagadoTarjeta = resultadoConsulta.getBoolean("pagado_tarjeta");
-            // Recoger el id de la caja del ticket
-            int caja_id = resultadoConsulta.getInt("caja_id");
-            // Prueba para imprimir los datos
-            System.out.println(id + "" + pagado + "" + pagadoTarjeta + caja_id);
-
-        }
-
+        return (nombre + "  " + precio);
     }
 
-    public static void consultarCaja(String consulta) throws SQLException {
-        // Se crea un dato de tipo sentencia con el metodo para conectarme a la BBDD
-        Statement sentencia = conexionBaseDatos();
+    public static String consultarDatosTickets(int id) throws SQLException {
+
+        ResultSet resultadoConsulta = SENTENCIA.executeQuery("meter una consulta" + id);
+
+        // Recoger datos de si está pagado
+        boolean pagado = resultadoConsulta.getBoolean("pagado");
+        // Recoger dato de si se ha pagado con tarjeta
+        boolean pagadoTarjeta = resultadoConsulta.getBoolean("pagado_tarjeta");
+        // Recoger el id de la caja del ticket
+        int caja_id = resultadoConsulta.getInt("caja_id");
+        // Prueba para imprimir los datos
+        return (pagado + "  " + pagadoTarjeta + "  " + caja_id);
+    }
+
+    public static String consultarDatosCaja(int id) throws SQLException {
+
         // Se almacena la salida de la consulta
-        ResultSet resultadoConsulta = sentencia.executeQuery(consulta);
+        ResultSet resultadoConsulta = SENTENCIA.executeQuery("Meter una consulta" + id);
 
-        System.out.println("id | Dinero | Fecha | Comentarios");
+        // Recoger datos del dinero que hay en la caja
+        double dinero = resultadoConsulta.getDouble("dinero");
+        // Recoger datos de la fecha en la que se hace caja
+        Date fecha = resultadoConsulta.getDate("fecha");
+        // Recoger los comentarios
+        String comentarios = resultadoConsulta.getString("comentarios");
 
-        while (resultadoConsulta.next()) {
-            // Recoger el id de la caja
-            int id = resultadoConsulta.getInt("id");
-            // Recoger datos del dinero que hay en la caja
-            double dinero = resultadoConsulta.getDouble("dinero");
-            // Recoger datos de la fecha en la que se hace caja
-            Date fecha = resultadoConsulta.getDate("fecha");
-            // Recoger los comentarios
-            String comentarios = resultadoConsulta.getString("comentarios");
-
-            System.out.println(id + " " + dinero + fecha + comentarios);
-
-        }
-
+        return (dinero + "  " + fecha + "  " + comentarios);
     }
 
-    public static void consultarTicket_productos(String consulta) throws SQLException {
+    public static String consultarDatosTicket_productos(int id) throws SQLException {
 
-        // Se crea un dato de tipo setencia con el metodo para conectarme a la BBDD
-        Statement sentencia = conexionBaseDatos();
         // Se almacena la salida de la consulta
-        ResultSet resultadoConsulta = sentencia.executeQuery(consulta);
+        ResultSet resultadoConsulta = SENTENCIA.executeQuery("meter una consulta" + id);
 
-        System.out.println("id | Ticket_id | Producto_id | Cantidad");
+        // Recoger el id del ticket
+        int ticket_id = resultadoConsulta.getInt("ticket_id");
+        // Recoger el id del producto
+        int producto_id = resultadoConsulta.getInt("producto_id");
 
-        while (resultadoConsulta.next()) {
-            // Recoger el id
-            int id = resultadoConsulta.getInt("id");
-            // Recoger el id del ticket
-            int ticket_id = resultadoConsulta.getInt("ticket_id");
-            // Recoger el id del producto
-            int producto_id = resultadoConsulta.getInt("producto_id");
-            // Recoger la cantidad de cada producto
-            int cantidad = resultadoConsulta.getInt("cantidad");
-
-            System.out.println(id + ticket_id + producto_id + cantidad);
-
-        }
-
+        return (ticket_id + "  " + producto_id);
     }
 
 }
